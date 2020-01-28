cc4b82aeb5179a5849aeb5837b89633a62f4b0d6
Alec Hoyland <alec.hoyland@posteo.net>
2020-01-28 11:36:53 -0500

    Helper function with hardcoded paths to test data

---
+++ utils/loadData.m
@@ -0,0 +1,23 @@
+function root = loadData(n)
+
+  % loads a grid cell from Holger's raw data
+  % requires that the cluster is mounted to /mnt/hasselmgrp
+  % the argument 'n' determines which file to load
+
+  prefix = '/mnt/hasselmogrp/hdannenb_1/UnitRecordingData/Grid_cells_lightVSdarkness_merged_across_sessions/';
+
+  raw_data = { ...
+    [prefix, 'ArchTWT14_181127_to_181210/Merged_sessions.mat'], ...
+    [prefix, 'ArchTChAT22_191030_to_191108/Cell1/merged_sessions_ArchTChAT#22_cell1.mat'], ...
+    [prefix, 'ArchTChAT22_191030_to_191108/Cell2/merged_sessions_ArchTChAT22_cell2.mat'], ...
+    [prefix, 'ArchTChAT22_191030_to_191108/Cell3/merged_session_ArchTChAT22_cell3.mat'], ...
+    [prefix, 'ArchTChAT22_191030_to_191108/Cell4/merged_session_ArchTChAT22_cell4.mat'] ...
+  };
+
+  if ~exist('n', 'var')
+    n = 1;
+  end
+
+  load(raw_data{n});
+
+end % function
