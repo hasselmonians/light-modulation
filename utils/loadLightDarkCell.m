b3161709cb101df796f5235fe1a207ff5cb828fe
Alec Hoyland <ahoyland@bu.edu>
2020-01-30 15:55:51 -0500

    Add documentation

--- utils/loadLightDarkCell.m
+++ utils/loadLightDarkCell.m
@@ -3,6 +3,12 @@ function [root, lightON, lightOFF] = loadLightDarkCell()
     % loads a light/dark cell from Holger's raw data
     % requires that the cluster is mounted to /mnt/hasselmogrp
 
+    % root is a CMBHOME.Session object
+    % lightON is a matrix of start and stop times in seconds
+    %   for when the light is on for the mouse
+    % lightOFF is a matrix of start and stop times in seconds
+    %   for when the light is off for the mouse
+
     load('/mnt/hasselmogrp/hdannenb_1/UnitRecordingData/ArchTChAT_18/190710_S1_LightVSdarkness_2min_30sec.mat');
     root.cel = [1, 1];
 
