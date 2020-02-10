30be5820d1cacda378a575d53ef3afef4516a5cd
Alec Hoyland <ahoyland@bu.edu>
2020-02-10 17:10:33 -0500

    Add function for padding spike counts

--- utils/overlayEpochs.m
+++ utils/overlayEpochs.m
@@ -1,6 +1,7 @@
 function [varargout] = overlayEpochs(root, epoch_times, varargin)
 
     %% Description:
+    %   Stacks epochs and gathers binned spike counts from those epochs.
     %
     %% Arguments:
     %   root: a CMBHOME.Session object
@@ -26,7 +27,7 @@ function [varargout] = overlayEpochs(root, epoch_times, varargin)
     %   [spike_counts, edges] = overlayEpochs(root, epoch_times, options);
     %   [spike_counts, edges] = overlayEpochs(root, epoch_times, 'Name', value, ...);
     %
-    % See Also: getEpochs, stitchEpochs
+    % See Also: getEpochs, stitchEpochs, padSpikeCounts
 
     %% Preamble
 
@@ -74,23 +75,6 @@ function [varargout] = overlayEpochs(root, epoch_times, varargin)
             end
         end
     end
-
-    % % extend spike counts and edges into matrices
-    % [nBins, bin_index] = max(cellfun('length', spike_counts));
-    %
-    % for ii = 1:length(spike_counts)
-    %     nPads = (nBins - length(spike_counts{ii})) / 2;
-    %     spike_counts{ii} = padarray(spike_counts{ii}, [0, nPads], NaN, 'both');
-    %     edges{ii} = padarray(edges{ii}, [0, nPads], NaN, 'both');
-    % end
-    %
-    % % convert spike counts and bin edges into matrices
-    % padded_spike_counts = cell2mat(spike_counts);
-    % padded_edges = cell2mat(edges);
-    %
-    % % find the mean and standard deviation of the spike counts
-    % mean_spike_counts = mean(padded_spike_counts, 1, 'omitnan');
-    % std_spike_counts = std(padded_spike_counts, 0, 1, 'omitnan');
 
     %% Outputs
 
---
+++ utils/padSpikeCounts.m
@@ -0,0 +1,45 @@
+function [padded_spike_counts, timestamps] = padSpikeCounts(spike_counts, edges, direction)
+
+    %% Description:
+    %   Stacks epochs and gathers binned spike counts from those epochs.
+    %
+    %% Arguments:
+    %   spike_counts: cell array of numerical vectors,
+    %       binned spike counts for each combined epoch
+    %   edges: cell array of numerical vectors
+    %       bin edges of the spike counts
+    %   direction: character vector
+    %       which direction to pad towards ('pre', 'post', or 'both')
+    %       default is 'both'
+    %
+    %% Outputs:
+    %   padded_spike_counts: numerical matrix
+    %       first dimension indexes the epochs
+    %       second dimension indexes the spike counts
+    %   timestamps: time bins for the spike counts, after padding
+    %       length is determined by the longest epoch
+    %
+    %% Examples:
+    %
+    % See Also: getEpochs, stitchEpochs, overlayEpochs
+
+    if ~exist('direction', 'var')
+        direction = 'both';
+    end
+
+    % find the maximum number of bins (determines the size of the padded matrix)
+    [nBins, bin_index] = max(cellfun('length', spike_counts));
+
+    % extend spike counts and edges into matrices
+    for ii = 1:length(spike_counts)
+        nPads = (nBins - length(spike_counts{ii})) / 2;
+        spike_counts{ii} = padarray(spike_counts{ii}, [0, nPads], NaN, direction);
+    end
+
+    % convert spike counts and bin edges into matrices
+    padded_spike_counts = cell2mat(spike_counts);
+    timestamps = edges{bin_index};
+
+    % % find the mean and standard deviation of the spike counts
+    % mean_spike_counts = mean(padded_spike_counts, 1, 'omitnan');
+    % std_spike_counts = std(padded_spike_counts, 0, 1, 'omitnan');
