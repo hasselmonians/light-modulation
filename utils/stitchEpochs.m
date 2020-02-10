b6c11de63b2bb96b127744e0b47c981ced8112db
Alec Hoyland <ahoyland@bu.edu>
2020-02-10 12:17:08 -0500

    Fix documentation

--- utils/stitchEpochs.m
+++ utils/stitchEpochs.m
@@ -10,13 +10,12 @@ function varargout = stitchEpochs(epoch_sets, varargin)
     %   epoch_sets: a 1x2 or 2x1 cell array of n x 2 matrices
     %       each matrix contains the start and stop times
     %       for each experimental condition (e.g. light and dark)
-    %   options: a struct of options, or
-    %   Name-Value argument pairs
+    %   options: a struct of options,
+    %       or as Name-Value pairs:
+    %           Verbosity: logical scalar, print textual output, default: false
     %
     %% Outputs:
-    %   options: a struct of options, which the function accepts as a single
-    %       struct argument, or as Name-Value pairs
-    %       Verbosity: logical scalar, print textual output, default: false
+    %   options: a struct of options
     %   stitched_epochs: an n x 3 matrix
     %       where the first column contains the starts of the light times,
     %       the second column contains the transition times,
