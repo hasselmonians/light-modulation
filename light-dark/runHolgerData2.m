57fd22bf6ac2cfb3bfd623d327a674247ee1c07a
Alec Hoyland <ahoyland@bu.edu>
2020-02-14 15:22:41 -0500

    Add new RatCatcher startup script and utility function

---
+++ light-dark/runHolgerData2.m
@@ -0,0 +1,36 @@
+r               = RatCatcher;
+r.localpath     = '/mnt/hasselmogrp/ahoyland/grid-cell-spiking/light-dark/cluster2';
+r.remotepath    = '/projectnb/hasselmogrp/ahoyland/grid-cell-spiking/light-dark/cluster2';
+r.protocol      = 'LightDark2';
+r.expID         = 'Holger';
+r.project       = 'hasselmogrp';
+r.verbose       = true;
+r.mode          = 'singular';
+
+%% Construct filename and filecode lists
+
+% load the raw data from the cluster
+data = load('/mnt/hasselmogrp/ahoyland/data/holger/data-Holger-LightDark.mat');
+
+% filter the raw data
+filtered_data_table = filterDataTable(data.data_table, ...
+    'p', 0.05, ...
+    'Modulation', 'positive', ...
+    'Mode', 'l2d');
+
+% use filenames and filecodes from the filtered data table
+r.filenames     = filtered_data_table.filenames;
+r.filecodes     = filtered_data_table.filecodes;
+
+return
+
+r.batchify
+
+%% Post-processing
+
+data_table = r.gather;
+
+%% Save results
+
+save('~/code/grid-cell-spiking/data/data-Holger-LightDark2.mat', 'data_table', 'r');
+!cp ~/code/grid-cell-spiking/data/data-Holger-LightDark2.mat /mnt/hasselmogrp/ahoyland/data/holger/
---
+++ utils/filterDataTable.m
@@ -0,0 +1,87 @@
+function varargout = filterDataTable(data_table, varargin)
+
+    %% Description:
+    %   Filters the data table produced by the LightDark protocol
+    %   to find cells that are modulated by the light/dark conditions.
+    %
+    %% Arguments:
+    %   data_table: the n x 10 data table created by the LightDark protocol
+    %   options: a struct of options, which the function accepts as a single
+    %       struct argument; or as Name-Value pairs
+    %       p: numerical scalar, the p-value threshold for significance, default: 0.05
+    %       Modulation: character vector, if 'positive', finds only positively-modulated cells,
+    %           if 'negative', finds only negatively-modulated cells,
+    %           if 'both', finds all modulated cells, default: 'both'
+    %       Mode: character vector, either 'l2d' (light to dark conditions),
+    %           or 'd2l' (dark to light) or 'both', default: 'both'
+    %       Verbosity: logical scalar, whether to print textual output, default: false
+    %
+    %% Outputs:
+    %   filtered_data_table: the filtered data table
+    %   I: logical index from data_table into filtered_data_table
+    %
+    %% Examples:
+    %   options = filterDataTable();
+    %   [filtered_data_table, I] = filterDataTable(data_table);
+    %   [filtered_data_table, I] = filterDataTable(data_table, options);
+    %   [filtered_data_table, I] = filterDataTable(data_table, 'Name', value, ...)
+    %
+    % See Also: RatCatcher.gather, RatCatcher.stitch, LightDark.batchFunction, getLightDarkStats
+
+    %% Preamble
+
+    options = struct;
+    options.p = 0.05;
+    options.Modulation = 'both';
+    options.Mode = 'l2d';
+    options.Verbosity = false;
+
+    if ~nargin & nargout
+        varargout{1} = options;
+        varargout = options;
+        options = orderfields(options);
+        return
+    end
+
+    options = corelib.parseNameValueArguments(options, varargin{:});
+
+    % assert options.Modulation
+    options.Modulation = lower(options.Modulation);
+    assert(any(strcmp(options.Modulation, {'positive', 'negative', 'both'})), ...
+        '''Modulation'' must be ''positive'', ''negative'', or ''both''');
+
+    % assert options.Mode
+    options.Mode = lower(options.Mode);
+    assert(any(strcmp(options.Mode, {'l2d', 'd2l', 'both'})), ...
+        '''Modulation'' must be ''l2d'', ''d2l'', or ''both''');
+
+    %% Filter the data table
+
+    switch options.Mode
+    case {'l2d', 'd2l'}
+        switch options.Modulation
+        case 'positive'
+            keep_these = data_table.([options.Mode '_p']) < options.p & data_table.([options.Mode '_tstat']) > 0;
+        case 'negative'
+            keep_these = data_table.([options.Mode '_p']) < options.p & data_table.([options.Mode '_tstat']) < 0;
+        case 'both'
+            keep_these = data_table.p < options.p;
+        end
+    case 'both'
+        switch options.Modulation
+        case 'positive'
+            keep_these = (data_table.l2d_p < options.p | data_table.d2l_p < options.p) & (data_table.l2d_tstat > 0 | data_table.d2l_tstat > 0);
+        case 'negative'
+            keep_these = (data_table.l2d_p < options.p | data_table.d2l_p < options.p) & (data_table.l2d_tstat < 0 | data_table.d2l_tstat < 0);
+        case 'both'
+            keep_these = (data_table.l2d_p < options.p | data_table.d2l_p < options.p);
+        end
+    end
+
+
+    %% Outputs
+
+    varargout{1} = data_table(keep_these, :);
+    varargout{2} = keep_these;
+
+end % function
