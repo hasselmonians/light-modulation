9f0fbcdcc371addb52cc51be36475a1fd99d47dc
Alec Hoyland <ahoyland@bu.edu>
2020-02-19 11:19:39 -0500

    Fix figures

--- scripts/average_over_epochs_cells.m
+++ scripts/average_over_epochs_cells.m
@@ -16,32 +16,35 @@ end
 
 %% Pad spike counts and compress into a matrix
 
-[padded_mean_binned_spike_counts, timestamps] = ...
-    padSpikeCounts(mean_binned_spike_counts, data_table.timestamps, 'both');
+padded_mean_binned_spike_counts = padSpikeCounts(mean_binned_spike_counts, data_table.timestamps, 'both');
 
 %% Average over padded mean binned spike counts
 % NOTE: z-score normalize before averaging
 
 zscored_spike_counts = zscoreOverNaNs(padded_mean_binned_spike_counts);
 
-return
+% create a timestamp vector
+binSize = 5; % seconds
+nBins = size(zscored_spike_counts, 2);
+zscored_timestamps = binSize * (colon(0, nBins-1) - nBins/2);
 
 %% Visualize
 
 % mean binned spike count (over all combined epochs & cells)
 % error-shaded with standard deviation
-figure;
 
-plotlib.shadedErrorBar(timestamps(1:end-1), these_spike_counts, these_errors);
+[vec_mean, vec_std] = averageOverNaNs(zscored_spike_counts);
+
+figure;
+plotlib.shadedErrorBar(zscored_timestamps, vec_mean, vec_std);
 xlabel('time (s)')
 ylabel('spikes/sec')
-title('mean binned spike counts (over all cells)')
+title('z-scored mean binned spike counts (over all cells)')
 figlib.pretty('PlotBuffer', 0.1);
 
 % z-scored mean binned spike count (over all combined epochs & cells)
 figure;
-z_spike_counts = (these_spike_counts - mean(these_spike_counts)) ./ these_errors;
-plot(timestamps(1:end-1), z_spike_counts);
+plot(zscored_timestamps, zscored_spike_counts);
 xlabel('time (s)')
 ylabel('a.u.')
 title('z-scored mean binned spike counts (over all cells)')
