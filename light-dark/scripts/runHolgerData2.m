114016a85533a6c5daad87e8b8bed5be3b1a1d0b
Alec Hoyland <ahoyland@bu.edu>
2020-02-20 13:06:13 -0500

    Add DarkLight

--- light-dark/scripts/runHolgerData2.m
+++ light-dark/scripts/runHolgerData2.m
@@ -1,3 +1,5 @@
+%% LightDark %%
+
 r = getSecondPassRatCatcher('LightDark');
 
 return
@@ -12,7 +14,30 @@ return
 
 data_table = LightDark2.gather(r);
 data_table = r.stitch(data_table);
+
 %% Save results
 
 save('~/code/grid-cell-spiking/data/data-Holger-LightDark2.mat', 'data_table', 'r');
 !cp ~/code/grid-cell-spiking/data/data-Holger-LightDark2.mat /mnt/hasselmogrp/ahoyland/data/holger/
+
+%% DarkLight %%
+
+r = getSecondPassRatCatcher('DarkLight');
+
+return
+
+%% Run on the cluster
+
+r.batchify
+
+return
+
+%% Post-processing
+
+data_table = LightDark2.gather(r);
+data_table = r.stitch(data_table);
+
+%% Save results
+
+save('~/code/grid-cell-spiking/data/data-Holger-DarkLight2.mat', 'data_table', 'r');
+!cp ~/code/grid-cell-spiking/data/data-Holger-DarkLight2.mat /mnt/hasselmogrp/ahoyland/data/holger/
