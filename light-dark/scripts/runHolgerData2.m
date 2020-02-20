3997b1803d8c3affad1b10836f53ec6b99dbe9c0
Alec Hoyland <ahoyland@bu.edu>
2020-02-20 12:05:58 -0500

    Remove duplicated code

--- light-dark/scripts/runHolgerData2.m
+++ light-dark/scripts/runHolgerData2.m
@@ -1,26 +1,4 @@
-r               = RatCatcher;
-r.localpath     = '/mnt/hasselmogrp/ahoyland/grid-cell-spiking/light-dark/cluster-LightDark2';
-r.remotepath    = '/projectnb/hasselmogrp/ahoyland/grid-cell-spiking/light-dark/cluster-LightDark2';
-r.protocol      = 'LightDark2';
-r.expID         = 'Holger';
-r.project       = 'hasselmogrp';
-r.verbose       = true;
-r.mode          = 'singular';
-
-%% Construct filename and filecode lists
-
-% load the raw data from the cluster
-data = load('/mnt/hasselmogrp/ahoyland/data/holger/data-Holger-LightDark.mat');
-
-% filter the raw data
-filtered_data_table = filterDataTable(data.data_table, ...
-    'p', 0.01, ...
-    'Modulation', 'positive', ...
-    'Mode', 'both');
-
-% use filenames and filecodes from the filtered data table
-r.filenames     = filtered_data_table.filenames;
-r.filecodes     = filtered_data_table.filecodes;
+r = getSecondPassRatCatcher('LightDark');
 
 return
 
@@ -33,8 +11,7 @@ return
 %% Post-processing
 
 data_table = LightDark2.gather(r);
-data_table = [data_table, table(r.filenames, r.filecodes, 'VariableNames', {'filenames', 'filecodes'})];
-
+data_table = r.stitch(data_table);
 %% Save results
 
 save('~/code/grid-cell-spiking/data/data-Holger-LightDark2.mat', 'data_table', 'r');
