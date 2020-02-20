c1e53e474705f543d4f542d15bbb622c117a92be
Alec Hoyland <ahoyland@bu.edu>
2020-02-20 11:57:19 -0500

    Add DarkLight2 visualization

--- light-dark/scripts/make_figures.m
+++ light-dark/scripts/make_figures.m
@@ -11,7 +11,34 @@ conditions = { ...
 
 for ii = 1:length(conditions)
 
+      %% LightDark %%
+
       load(['~/code/grid-cell-spiking/data/data-Holger-LightDark2-' num2str(ii) '.mat']);
+
+      [zscored_spike_counts, zscored_timestamps] = averageOverEpochsCells(data_table);
+
+      [vec_mean, vec_std] = averageOverNaNs(zscored_spike_counts);
+
+      fig_handles(2*ii-1) = figure;
+      plotlib.shadedErrorBar(zscored_timestamps, vec_mean, vec_std);
+      xlabel('time (s)')
+      ylabel('z-scored firing rate (a.u.)')
+      title({'z-scored mean binned spike counts (over all cells)'; ...
+          conditions{ii}})
+      figlib.pretty('PlotBuffer', 0.1);
+
+      % z-scored mean binned spike count (over all combined epochs & cells)
+      fig_handles(2*ii) = figure;
+      plot(zscored_timestamps, zscored_spike_counts);
+      xlabel('time (s)')
+      ylabel('z-scored firing rate (a.u.)')
+      title({'z-scored mean binned spike counts (over all cells)'; ...
+          conditions{ii}})
+      figlib.pretty('PlotBuffer', 0.1);
+
+      %% DarkLight %%
+
+      load(['~/code/grid-cell-spiking/data/data-Holger-DarkLight2-' num2str(ii) '.mat']);
 
       [zscored_spike_counts, zscored_timestamps] = averageOverEpochsCells(data_table);
 
