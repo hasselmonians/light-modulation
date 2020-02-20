7e7db31e6d218fe01d58a521f7f4108d9057f2f7
Alec Hoyland <ahoyland@bu.edu>
2020-02-20 09:18:46 -0500

    Finish script

--- light-dark/scripts/runAllConditions.m
+++ light-dark/scripts/runAllConditions.m
@@ -1,6 +1,36 @@
 % set up jobs on the cluster for all four conditions
+% gather the data afterwards
+% save the data and copy to cluster
+
+%% Instantiate the RatCatcher objects
 
 for ii = 4:-1:1
-    r(ii) = getSecondPassRatCatcher(ii);
-    r(ii) = r(ii).batchify;
+    rc(ii) = getSecondPassRatCatcher(ii);
+    rc(ii) = rc(ii).validate;
 end
+
+return
+
+%% Batch the jobs
+
+for ii = 1:4
+    rc(ii).batchify;
+end
+
+return
+
+%% Gather the output data
+
+for ii = 1:4
+    r = rc(ii);
+    data_table = LightDark2.gather(r);
+    data_table = r.stitch(data_table);
+    save_filepath = '~/code/grid-cell-spiking/data/data-';
+    save([save_filepath, r.batchname], 'data_table', 'r');
+end
+
+return
+
+%% Copy to cluster
+
+!cp ~/code/grid-cell-spiking/data/data* /mnt/hasselmogrp/ahoyland/data/holger/
