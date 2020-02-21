f196e4f978889b191c92c8665edced65c4b0e96d
Alec Hoyland <ahoyland@bu.edu>
2020-02-20 19:02:21 -0500

    Determine when datasets converge

--- light-dark/tests/test_lightdark.m
+++ light-dark/tests/test_lightdark.m
@@ -26,3 +26,32 @@ for ii = 1:height(data_lightdark2.data_table)
 end
 
 % No they aren't different at all...
+
+%% How about the filtered datasets?
+
+p = 0.01;
+modulation = 'positive';
+
+% load the raw data from the cluster
+data_lightdark = load('/mnt/hasselmogrp/ahoyland/data/holger/data-Holger-LightDark.mat');
+data_darklight = load('/mnt/hasselmogrp/ahoyland/data/holger/data-Holger-DarkLight.mat');
+
+% filter the raw data
+filtered_lightdark = filterDataTable(data_lightdark.data_table, ...
+    'p', p, ...
+    'Modulation', modulation, ...
+    'Mode', 'both');
+filtered_darklight = filterDataTable(data_darklight.data_table, ...
+    'p', p, ...
+    'Modulation', modulation, ...
+    'Mode', 'both');
+
+%% Are the two data sets actually different?
+
+% They are not different.
+differences = setdiff(filtered_lightdark, filtered_darklight);
+isempty(differences)
+
+height(differences) / height(filtered_lightdark) * 100
+
+% This means that the error is in filtering.
