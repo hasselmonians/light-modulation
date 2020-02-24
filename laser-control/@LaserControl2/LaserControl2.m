a14bc86194c9f7f16794f2df477495aef4af0e74
Alec Hoyland <ahoyland@bu.edu>
2020-02-24 15:43:11 -0500

    Add LaserControl2

---
+++ laser-control/@LaserControl2/LaserControl2.m
@@ -0,0 +1,17 @@
+classdef LaserControl2
+
+properties
+
+end % properties
+
+methods
+
+end % methods
+
+methods (Static)
+
+    batchFunction(location, batchname, outfile, test)
+
+end % static methods
+
+end % classdef
