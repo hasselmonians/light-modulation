b6e0c47a9fb30d7d5f438f394d6398fa0d0049be
Alec Hoyland <ahoyland@bu.edu>
2020-02-26 17:25:49 -0500

    Fix paths

--- laser-control/scripts/runHolgerDataFirstPass.m
+++ laser-control/scripts/runHolgerDataFirstPass.m
@@ -15,4 +15,4 @@ data_table = r.stitch(data_table);
 return
 
 save([['~/code/light-modulation/data/data-Holger-' r.protocol '.mat']], 'data_table', 'r');
-% !cp ~/code/light-modulation/data/holger/data-Holger-LaserControl.mat /mnt/hasselmogrp/ahoyland/data/
+% !cp ~/code/light-modulation/data/data-Holger-LaserControl.mat /mnt/hasselmogrp/ahoyland/data/holger/
