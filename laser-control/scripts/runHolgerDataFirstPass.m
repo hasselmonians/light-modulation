5efd812abe5cc55ff8c5414bc2998e1c29805f6e
Alec Hoyland <ahoyland@bu.edu>
2020-02-26 16:38:08 -0500

    Use first pass function

--- laser-control/scripts/runHolgerDataFirstPass.m
+++ laser-control/scripts/runHolgerDataFirstPass.m
@@ -1,16 +1,7 @@
-r               = RatCatcher;
-r.localpath     = '/mnt/hasselmogrp/ahoyland/light-modulation/laser-control/cluster';
-r.remotepath    = '/projectnb/hasselmogrp/ahoyland/light-modulation/laser-control/cluster';
-r.protocol      = 'LaserControl';
-r.expID         = 'Holger';
-r.project       = 'hasselmogrp';
-r.verbose       = true;
-r.mode          = 'singular';
+protocol = 'LaserControl';
+% protocol = 'ControlLaser';
 
-r.filenames     = '/mnt/hasselmogrp/ahoyland/data/holger/laser-control/filenames.txt';
-r.filecodes     = '/mnt/hasselmogrp/ahoyland/data/holger/laser-control/filecodes.csv';
-
-r = r.validate;
+r = getFirstPassRatCatcher(protocol);
 
 return
 
@@ -23,5 +14,5 @@ data_table = r.stitch(data_table);
 
 return
 
-save('~/code/light-modulation/data/data-Holger-LaserControl.mat', 'data_table', 'r');
+save([['~/code/light-modulation/data/data-Holger-' r.protocol '.mat']], 'data_table', 'r');
 % !cp ~/code/light-modulation/data/holger/data-Holger-LaserControl.mat /mnt/hasselmogrp/ahoyland/data/
